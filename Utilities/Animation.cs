1cc81c473eeef8c866c5506a0e9fc18683aa2bda
nehpe <n@nehpe.com>
2018-02-15 22:30:30 -0700

    Added animation, fireball and portal content

---
+++ Utilities/Animation.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameJam.Utilities
+{
+    public class Animation
+    {
+        public int tileSize = 16;
+        public float speed = 10;
+        public int currentFrame = 0;
+        public int totalFrames = 0;
+        public Rectangle StartRect;
+
+        private float _timer = 0f;
+
+        public Rectangle Rectangle { get
+            {
+                return new Rectangle(
+                    StartRect.X + (currentFrame * tileSize), StartRect.Y,
+                    tileSize, tileSize
+                    );
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _timer += (float) gameTime.ElapsedGameTime.TotalMilliseconds / 16f;
+            if (_timer > speed)
+            {
+                currentFrame++;
+                _timer = 0f;
+                if (currentFrame+1 == totalFrames)
+                {
+                    currentFrame = 0;
+                }
+            }
+        }
+    }
+
+    public enum AnimationDirection
+    {
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        NORTHEAST,
+        NORTHWEST,
+        SOUTHEAST,
+        SOUTHWEST
+    }
+}
